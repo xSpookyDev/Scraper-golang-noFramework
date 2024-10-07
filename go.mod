cc6ffec346c19f5c5167dee9d4414f442aaf03cf
xSpookyDev <ricardo13dev@gmail.com>
2024-10-07 01:52:26 -0300

    finalizado

---
+++ go.mod
@@ -0,0 +1,5 @@
+module simple-scraper
+
+go 1.21.5
+
+require golang.org/x/net v0.30.0 // indirect
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func getHref(t html.Token) (ok bool, href string) {
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+	return
+}
+
+func crawl(url string, ch chan string, chFinished chan bool) {
+	resp, err := http.Get(url)
+
+	defer func() {
+		chFinished <- true
+	}()
+
+	if err != nil {
+		fmt.Println("Error: failed to crawl", url)
+		return
+	}
+
+	b := resp.Body
+	defer b.Close()
+
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		switch {
+
+		case tt == html.ErrorToken:
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			isTag := t.Data == "a"
+			if !isTag {
+				continue
+			}
+			ok, url := getHref(t)
+
+			if !ok {
+				continue
+			}
+			hasProto := strings.Index(url, "http") == 0
+			if hasProto {
+				ch <- url
+			}
+		}
+	}
+}
+
+func main() {
+
+	foundUrls := make(map[string]bool)
+	seedUrls := os.Args[1:]
+
+	chUrls := make(chan string)
+	chFinished := make(chan bool)
+
+	for _, url := range seedUrls {
+		go crawl(url, chUrls, chFinished)
+	}
+
+	for c := 0; c < len(seedUrls); {
+		select {
+		case url := <-chUrls:
+			foundUrls[url] = true
+		case <-chFinished:
+			c++
+		}
+	}
+	fmt.Println("\nFound", len(foundUrls), "Unique urls")
+
+	for url := range foundUrls {
+		fmt.Println("-" + url)
+	}
+	close(chUrls)
+}
